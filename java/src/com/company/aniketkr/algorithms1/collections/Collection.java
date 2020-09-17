afeac049992dbf04e1b70aa1d6dfeeb8c646c5e9
Aniket Kumar <aniket24kr@gmail.com>
2020-09-17 10:16:47 +0530

    added methods to Collection interface

--- java/src/com/company/aniketkr/algorithms1/collections/Collection.java
+++ java/src/com/company/aniketkr/algorithms1/collections/Collection.java
@@ -1,4 +1,18 @@
 package com.company.aniketkr.algorithms1.collections;
 
+import java.util.Iterator;
+
 public interface Collection<E> extends Iterable<E> {
+
+  int size();
+
+  boolean isEmpty();
+
+  void clear();
+
+  Collection<E> copy();
+
+  Iterator<E> iterator();
+
+  boolean equals(Object obj);
 }
