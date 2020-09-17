7f14a9075bd202bcf7839bc7a7ed9d8e7ffa6298
Aniket Kumar <aniket24kr@gmail.com>
2020-09-17 15:06:01 +0530

    implemented utility class Sorter.java

--- java/src/com/company/aniketkr/algorithms1/sorting/Sorter.java
+++ java/src/com/company/aniketkr/algorithms1/sorting/Sorter.java
@@ -1,4 +1,36 @@
 package com.company.aniketkr.algorithms1.sorting;
 
-abstract class Sorter {
+class Sorter {
+
+  static void swap(Object[] arr, int i, int j) {
+    Object tmp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = tmp;
+  }
+
+  static void checkIndex(Object[] arr, int i) {
+    if (i < 0 || i > arr.length) {
+      throw new IndexOutOfBoundsException("index out of range: " + i);
+    }
+  }
+
+  static void shuffle(Object[] arr) {
+    int randIndex;
+    for (int i = arr.length - 1; i > 0; i--) {
+      randIndex = random(0, i + 1);
+      swap(arr, i, randIndex);
+    }
+  }
+
+  /* **************************************************************************
+   * Section: Utility Methods
+   ************************************************************************** */
+
+  private static int random(int low, int high) {
+    if (low >= high) {
+      throw new IllegalArgumentException("`low` can't be equal to or higher that `high`");
+    }
+
+    return low + (int) (Math.random() * (high - low));
+  }
 }
