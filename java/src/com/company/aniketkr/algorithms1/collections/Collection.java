e3ba009ad881dc133058f85331aeb314e17091b3
Aniket Kumar <aniket24kr@gmail.com>
2020-09-21 07:41:42 +0530

    updated docs and reorganised code (Collections.java)

--- java/src/com/company/aniketkr/algorithms1/collections/Collection.java
+++ java/src/com/company/aniketkr/algorithms1/collections/Collection.java
@@ -1,24 +1,79 @@
 package com.company.aniketkr.algorithms1.collections;
 
 import java.util.Iterator;
+import java.util.function.Function;
 
 /**
  * A {@link Collection} represents a collection of objects of the same type. Every
  * collection object supports the methods <em>size</em>, <em>is-empty</em>,
- * <em>clear</em> and <em>copy</em>. Apart from these methods, all collection abjects
+ * <em>clear</em> and <em>copy</em>. Apart from these methods, all collection objects
  * guarantee overriding {@link Object#equals(Object obj) equals} and
- * {@link Object#toString() toString} Object methods. All collections are naturally iterable
+ * {@link Object#toString() toString} Object methods. Collections are naturally iterable
  * and hence implement the {@link Iterable} interface.
  *
- * @param <E> The type of elements in the collection.
+ * @param <E> The type of element in the collection.
  * @author Aniket Kumar
  */
 public interface Collection<E> extends Iterable<E> {
 
+  /* **************************************************************************
+   * Section: Object Methods
+   ************************************************************************** */
+
+  /**
+   * Is the collection equal to the given object {@code obj}?
+   * A collection will be deemed equal to {@code obj} if {@code obj} satisfies the following
+   * conditions:
+   * <ol>
+   *   <li>{@code obj} is a {@link Collection} object or one of its subclasses.</li>
+   *   <li>
+   *     All the elements of {@code obj} are equal to the corresponding elements in this
+   *     collection.
+   *   </li>
+   * </ol>
+   *
+   * <p>
+   *   Because of Java's use of erasure, it is currently not possible to differentiate
+   *   between the type of elements in two empty collections. This implies that if two
+   *   empty {@link Collection} objects - which store two different types of elements, are
+   *   compared with {@code equals()}, then the result will always be {@code true}.
+   * </p>
+   *
+   * @param obj The other Object to compare {@code this} with for equality.
+   * @return {@code true} if {@code obj} is equal to this collection,
+   *     {@code false} otherwise.
+   */
+  boolean equals(Object obj);
+
+  /**
+   * Get a string representation of the collection.
+   * Primarily for debugging and helping find bugs in client code.
+   *
+   * @return A string.
+   */
+  String toString();
+
+  /* **************************************************************************
+   * Section: Iterable Methods
+   ************************************************************************** */
+
+  /**
+   * Get an iterator that produces the elements of the collection in its <em>natural
+   * order</em>.
+   * The natural order is best defined by the implementing subclass.
+   *
+   * @return An iterator.
+   */
+  Iterator<E> iterator();
+
+  /* **************************************************************************
+   * Section: Collection Methods
+   ************************************************************************** */
+
   /**
    * How many elements are present in the collection?
    *
-   * @return The count of number of elements in the collection.
+   * @return The count of elements present in the collection.
    */
   int size();
 
@@ -30,57 +85,38 @@ public interface Collection<E> extends Iterable<E> {
   boolean isEmpty();
 
   /**
-   * Clear the collection of all its elements. Set it to its instantiated state.
-   */
-  void clear();
-
-  /**
    * Does {@code elmt} exist in the collection?
    *
-   * @param elmt The element to check for.
-   * @return {@code true} if {@code elmt} exists in the collection, {@code false}
-   *     otherwise.
-   * @throws IllegalArgumentException If {@code elmt} is {@code null}.
+   * @param elmt The element to check existence of.
+   * @return {@code true} if {@code elmt} exists, {@code false} otherwise.
    */
   boolean contains(E elmt);
 
   /**
-   * Get a shallow copy of the collection.
+   * Clear the collection of all its elements.
+   * Sets all internal state members to their <em>default</em> initial state.
+   */
+  void clear();
+
+  /**
+   * Return a shallow copy of the collection.
+   * A shallow copy creates a copy of the collection but not the elements in the
+   * collection.
    *
-   * @return A new collection.
+   * @return A shallow copy of the collection.
    */
   Collection<E> copy();
 
   /**
-   * Get an iterator object that produces the elements of the collection in its natural
-   * order.
+   * Returns a deepcopy of the collection.
+   * A deepcopy creates a copy of the collection and populates it with copies of the
+   * original elements.
    *
-   * @return An iterator.
-   */
-  Iterator<E> iterator();
-
-  /* **************************************************************************
-   * Section: Object Methods
-   ************************************************************************** */
-
-  /**
-   * Check if this collection is equal to the given object.
-   * Calls {@link Object#equals(Object that) equals()} on the elements to check if they
-   * are equal.
+   * @param copyFn A {@link Function} that takes original element as the
+   *               argument and returns a deepcopy of that element.
+   * @return A deepcopy of the collection.
    *
-   * @param obj The other object to check for equality.
-   * @return {@code true} if {@code obj} is semantically equal to this collection,
-   *     {@code false} otherwise.
-   * @implSpec As it currently stands, if two collection objects are empty, the {@code equals}
-   *     method will always return {@code true}. This happens as no elements are available to
-   *     compare.
+   * @throws IllegalArgumentException If {@code copyFn} is {@code null}.
    */
-  boolean equals(Object obj);
-
-  /**
-   * Return a string representation of the collection. Primarily for debugging.
-   *
-   * @return A string.
-   */
-  String toString();
+  Collection<E> deepcopy(Function<? super E, E> copyFn);
 }
